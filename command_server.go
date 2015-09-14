32a04cfbc8938a33955922c97079bc2e3e8a7ba9
Jais Cheema <jaischeema@gmail.com>
2015-09-14 21:48:57 +1000

    Add single photo endpoint

--- command_server.go
+++ command_server.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	// "fmt"
 	"github.com/codegangsta/cli"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
-	_ "github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm"
 	"github.com/unrolled/render"
 	"net/http"
 )
 
 var Render = render.New()
+var DB gorm.DB
 
-func HomeHandler(w http.ResponseWriter, req *http.Request) {
-	Render.JSON(w, http.StatusOK, map[string]string{"hello": "world"})
+func PhotosHandler(response http.ResponseWriter, request *http.Request) {
+	Render.JSON(response, http.StatusOK, map[string]string{"hello": "world"})
+}
+
+func PhotoHandler(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id := vars["id"]
+
+	var photo Photo
+	DB.Preload("SimilarPhotos").First(&photo, id)
+	Render.JSON(response, http.StatusOK, photo)
+}
+
+func SimilarPhotosHandler(response http.ResponseWriter, request *http.Request) {
+	Render.JSON(response, http.StatusOK, map[string]string{"hello": "world"})
 }
 
 func RunServer(c *cli.Context) {
 	router := mux.NewRouter()
-	router.HandleFunc("/", HomeHandler)
+	DB = SetupDatabase(c)
+
+	router.HandleFunc("/photos", PhotosHandler)
+	router.HandleFunc("/photos/{id}", PhotoHandler)
+	router.HandleFunc("/similar", SimilarPhotosHandler)
 
 	server := negroni.Classic()
 	server.UseHandler(router)
