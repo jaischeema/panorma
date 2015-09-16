fd74fc91d0a584486c2d469467848dec4a04fdb6
Jais Cheema <jaischeema@gmail.com>
2015-09-16 21:39:17 +1000

    Add json annotations for the models

--- models.go
+++ models.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"github.com/jinzhu/gorm"
 	"time"
+
+	"github.com/jinzhu/gorm"
 )
 
 type SimilarPhoto struct {
-	Id             int64
-	PhotoId        int64
-	SimilarPhotoId int64
+	Id             int64 `json:"id"`
+	PhotoId        int64 `json:"photo_id"`
+	SimilarPhotoId int64 `json:"similar_photo_id"`
 }
 
 type Photo struct {
-	Id            int64
-	Path          string `sql:"not null;unique"`
-	HashValue     int64  `sql:"not null"`
-	TakenAt       time.Time
-	Lat           float64
-	Lng           float64
-	Height        int
-	Width         int
-	Size          int64
-	SimilarPhotos []SimilarPhoto
+	Id            int64          `json:"id"`
+	Path          string         `json:"path";sql:"not null;unique"`
+	HashValue     int64          `json:"hash_value";sql:"not null"`
+	TakenAt       time.Time      `json:"taken_at"`
+	Lat           float64        `json:"latitude"`
+	Lng           float64        `json:"longitude"`
+	Height        int            `json:"height"`
+	Width         int            `json:"width"`
+	Size          int64          `json:"size"`
+	SimilarPhotos []SimilarPhoto `json:"similar_photos"`
 }
 
 func (photo Photo) ExistsInDatabase(db gorm.DB) bool {
