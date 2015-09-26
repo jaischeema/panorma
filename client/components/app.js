ed7bf223f5ee2dc3d11233bf15ce69e20cefcf4c
Jais Cheema <jaischeema@gmail.com>
2015-09-26 15:41:02 +1000

    Add sidebar to the app along with some simple container styling

--- client/components/app.js
+++ client/components/app.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React   from 'react';
+import Sidebar from './sidebar';
+
+require('../styles/app.scss');
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>Panorma</h1>
-        {this.props.children}
+        <Sidebar />
+        <div className="content">
+          {this.props.children}
+        </div>
       </div>
     );
   }
